71882fcf2848a43d9aea7366d3d473c1048d4fe3
Oguzhan Saman <oguzhan.saman@themos.com.tr>
2025-08-25 11:10:25 +0300

    Integrate Decisions app into project settings and update URL routing for decisions endpoint

--- turkishhelper/turkishhelper/urls.py
+++ turkishhelper/turkishhelper/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scrapper/', include('Scrapper.urls')),
+    path('decisions/', include('Decisions.urls')),
 ]
